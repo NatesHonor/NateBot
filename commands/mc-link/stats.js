a7c45a6890850c9efc54180deb9a99dfc12d502b
Nate <nathaniel.j.marcellus@gmail.com>
2023-08-04 20:18:01 -0400

    Fixed and finished bot

---
+++ commands/mc-link/stats.js
@@ -0,0 +1,80 @@
+const { SlashCommandBuilder } = require('discord.js');
+const mysql = require('mysql');
+const config = require('../../config.json');
+const fetch = require('node-fetch');
+const { EmbedBuilder } = require('discord.js'); // Make sure to import EmbedBuilder if not already imported
+
+const connection = mysql.createConnection({
+  host: config.host,
+  user: config.username,
+  password: config.password,
+  database: config.database,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL database:', err);
+  }
+});
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('stats')
+    .setDescription('Displays profile information'),
+  async execute(interaction) {
+    const discordUserId = interaction.user.id;
+
+    const sql = 'SELECT minecraft_username FROM account_links WHERE discord_user_id = ?';
+    connection.query(sql, [discordUserId], async (err, result) => {
+      if (err) {
+        console.error('Error querying database:', err);
+        return interaction.reply('An error occurred while fetching the profile. Please try again later.');
+      }
+
+      if (result.length === 0) {
+        return interaction.reply('You have not linked your Minecraft account.');
+      }
+
+      const minecraftUsername = result[0].minecraft_username;
+
+      try {
+        const response = await fetch(`http://localhost:3000/player/${encodeURIComponent(minecraftUsername)}`);
+        const data = await response.json();
+
+        // Check if the data contains the expected fields with the correct types
+        if (
+          !data ||
+          typeof data !== 'object' ||
+          typeof data.rank !== 'string' ||
+          typeof data.level !== 'number' ||
+          typeof data.avatarUrl !== 'string' ||
+          (data.playtime && typeof data.playtime !== 'string')
+        ) {
+          console.error('Invalid data received from the API:', data);
+          return interaction.reply('An error occurred while fetching the profile. Please try again later.');
+        }
+
+        const rank = data.rank;
+        const level = data.level.toString(); // Convert the level value to a string
+        const avatarUrl = data.avatarUrl;
+        const playtime = data.playtime || 'N/A';
+
+        const embed = new EmbedBuilder()
+          .setColor(rank.toLowerCase() === 'admin' ? '#FF0000' : '#0099ff')
+          .setTitle('Profile Information')
+          .addFields(
+            { name: 'Rank', value: rank, inline: true },
+            { name: 'Minecraft Username', value: minecraftUsername, inline: true },
+            { name: 'Level', value: level, inline: true },
+            { name: 'Playtime', value: playtime, inline: true },
+          )
+          .setThumbnail(avatarUrl);
+
+        interaction.reply({ embeds: [embed] });
+      } catch (error) {
+        console.error('Error fetching player data:', error);
+        interaction.reply('An error occurred while fetching the profile. Please try again later.');
+      }
+    });
+  },
+};
--- data/MySQL/create-tables.js
+++ data/MySQL/create-tables.js
@@ -9,51 +9,69 @@ const connection = mysql.createConnection({
 });
 
 function createTables() {
-    connection.connect((err) => {
-      if (err) {
-        console.error('Error connecting to MySQL database:', err);
-        return;
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL database:', err);
+      return;
+    }
+
+    const tables = [
+      {
+        name: 'reports',
+        query: `CREATE TABLE IF NOT EXISTS reports (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                platform VARCHAR(255) NOT NULL,
+                reporter_id VARCHAR(255) NOT NULL,
+                user VARCHAR(255) NOT NULL,
+                reason TEXT NOT NULL,
+                evidence TEXT,
+                report_key VARCHAR(36) NOT NULL UNIQUE,
+                claimed TINYINT(1) DEFAULT 0
+              )`,
+      },
+      {
+        name: 'tickets',
+        query: `CREATE TABLE IF NOT EXISTS tickets (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                user_id VARCHAR(255) NOT NULL,
+                guild_id VARCHAR(255) NOT NULL,  -- Add the guild_id column
+                channel_id VARCHAR(255) NOT NULL,
+                message TEXT NOT NULL,
+                is_closed TINYINT(1) DEFAULT 0
+              )`,
+      },
+      {
+        name: 'tokens',
+        query: `CREATE TABLE IF NOT EXISTS tokens (
+          id INT AUTO_INCREMENT PRIMARY KEY,
+          discord_user_id VARCHAR(18) NOT NULL,
+          token VARCHAR(32) NOT NULL,
+          expire_at BIGINT NOT NULL
+        )`,
+      },
+      {
+        name: 'account_links',
+        query: `CREATE TABLE IF NOT EXISTS account_links (
+          id INT AUTO_INCREMENT PRIMARY KEY,
+          discord_user_id VARCHAR(255) NOT NULL,
+          minecraft_username VARCHAR(255) NOT NULL,
+          \`rank\` VARCHAR(50) NOT NULL,  -- Backticks around 'rank'
+          playtime VARCHAR(50) NOT NULL
+        )`,
       }
-  
-      const tables = [
-        {
-          name: 'reports',
-          query: `CREATE TABLE IF NOT EXISTS reports (
-                  id INT AUTO_INCREMENT PRIMARY KEY,
-                  platform VARCHAR(255) NOT NULL,
-                  reporter_id VARCHAR(255) NOT NULL,
-                  user VARCHAR(255) NOT NULL,
-                  reason TEXT NOT NULL,
-                  evidence TEXT,
-                  report_key VARCHAR(36) NOT NULL UNIQUE,
-                  claimed TINYINT(1) DEFAULT 0
-                )`,
-        },
-        {
-          name: 'tickets',
-          query: `CREATE TABLE IF NOT EXISTS tickets (
-                  id INT AUTO_INCREMENT PRIMARY KEY,
-                  user_id VARCHAR(255) NOT NULL,
-                  guild_id VARCHAR(255) NOT NULL,  -- Add the guild_id column
-                  channel_id VARCHAR(255) NOT NULL,
-                  message TEXT NOT NULL,
-                  is_closed TINYINT(1) DEFAULT 0
-                )`,
-        },
-      ];
-  
-      tables.forEach((table) => {
-        connection.query(table.query, (err) => {
-          if (err) {
-            console.error(`Error creating table ${table.name}:`, err);
-          } else {
-            console.log(`Table ${table.name} created successfully.`);
-          }
-        });
+    ];
+
+    tables.forEach((table) => {
+      connection.query(table.query, (err) => {
+        if (err) {
+          console.error(`Error creating table ${table.name}:`, err);
+        } else {
+        }
       });
-  
-      connection.end();
     });
-  }
+
+    connection.end();
+  });
+}
   
   module.exports = createTables;
