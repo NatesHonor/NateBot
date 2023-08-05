80752379bb8501c281e5d0c470ecd73ae581898f
Nate <nathaniel.j.marcellus@gmail.com>
2023-08-05 09:01:20 -0400

    Added embed and finalized command

--- commands/leveling/level.js
+++ commands/leveling/level.js
@@ -1,5 +1,6 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const getUserData = require('../../data/MySQL/functions/getUserData');
+const calculateExpRequiredForLevel = require('../../data/MySQL/functions/calculateExpRequiredForLevel');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -14,12 +15,33 @@ module.exports = {
         return interaction.reply('Your level data is not available.');
       }
 
-      const level = userData.level || 1;
+      const currentExp = userData.exp || 0;
+      const currentLevel = userData.level || 1;
+      const expNeededForNextLevel = calculateExpRequiredForLevel(currentLevel + 1);
+      const progress = (currentExp / expNeededForNextLevel) * 100;
 
-      interaction.reply(`Your current level is: ${level}`);
+      const embed = new EmbedBuilder()
+        .setColor('#0099ff')
+        .setTitle('Level Information')
+        .setDescription(`You are currently at level ${currentLevel}!`)
+        .addFields(
+          { name: 'Progress to Level ' + (currentLevel + 1), value: `${currentExp}/${expNeededForNextLevel} XP`, inline: true },
+          { name: '\u200B', value: '\u200B', inline: true },
+          { name: 'Progress Bar', value: getProgressBar(progress), inline: true }
+        );
+
+      interaction.reply({ embeds: [embed] });
     } catch (error) {
       console.error('Error fetching player data:', error);
       interaction.reply('An error occurred while fetching the level. Please try again later.');
     }
   },
 };
+
+// Helper function to create the progress bar
+function getProgressBar(progress) {
+  const progressBarLength = 20;
+  const filledBlocks = Math.round(progressBarLength * (progress / 100));
+  const emptyBlocks = progressBarLength - filledBlocks;
+  return `${'▓'.repeat(filledBlocks)}${'░'.repeat(emptyBlocks)} [${progress.toFixed(1)}%]`;
+}
